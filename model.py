ed210466f285407a50995876b05d8a061d47878d
ShafiSahal99 <shafisahal99@gmail.com>
2021-05-18 17:39:43 +0530

    completed weight initialization

--- model.py
+++ model.py
@@ -16,3 +16,21 @@ def normalized_columns_initializer(weights, std = 1.0):
     out *= std / torch.sqrt(out.pow(2).sum(1).expand_as(out))
     return out
 
+def weights_init(m):
+    classname = m.__classname__.__name__
+    
+    if classname.find('Conv') != -1:
+        weight_shape = list(m.weight.data.size())
+        fan_in = np.prod(weight_shape[1:4])
+        fan_out = np.prod(weigh_shape[2:4]) * weigh_shape[0]
+        w_bound = np.sqrt(6. / (fan_in + fan_out))
+        m.weight.data.uniform(-w_bound + w_bound)
+        m.bias.data.fill_(0)
+    elif classname.find('Linear') != -1:
+        weight_shape = list(m.weight.data.size())
+        fan_in = weight_shape[1]
+        fan_out = weight_shape[0]
+        w_bound = np.sqrt(6. / (fan_in + fan_out))
+        m.weight.data.uniform(-w_bound + w_bound)
+        m.bias.data.fill_(0)
+        
